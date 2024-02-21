0552ce4ad8b731f91df2e7277b5bb1b129c6ba60
yezixx <skylow917@daum.net>
2024-02-21 21:42:37 +0900

    파일첨부_파일요청 보내기 - save.html 수정 - boardDTO 수정

--- README.md
+++ README.md
@@ -11,10 +11,20 @@
 2. 글목록(/board/)
 3. 글조회(/board/{id})
 4. 글수정(/board/update/{id})
-- 상세 화면에서 수정 버튼 클릭
-- 서버에서 해당 게시글의 정보를 가지고 수정 화면 출력
-- 제목, 내용 수정 입력 받아서 서버로 요청
-- 수정 처리
+   - 상세 화면에서 수정 버튼 클릭
+   - 서버에서 해당 게시글의 정보를 가지고 수정 화면 출력
+   - 제목, 내용 수정 입력 받아서 서버로 요청
+   - 수정 처리
 5. 글삭제(/board/delete/{id})
 6. 페이징 처리(/board/paging/)
-- /board/paging?page=2
+   - /board/paging?page=2
+7. 파일(이미지) 첨부하기
+    - 단일 파일 첨부
+    - 다중 파일 첨부
+    - 파일 첨부와 관련하여 추가될 부분들
+      - save.html
+      - BoardDTO
+      - BoardService.save()
+      - BoardEntity
+      - BoardFileEntity, BoardFileRepository 추가
+      - detail.html
--- src/main/java/com/example/spring_board/dto/BoardDTO.java
+++ src/main/java/com/example/spring_board/dto/BoardDTO.java
@@ -2,6 +2,7 @@ package com.example.spring_board.dto;
 
 import com.example.spring_board.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,11 @@ public class BoardDTO {
     private int boardHits; // 게시글 조회수
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+    
+    private MultipartFile boardFile; // save.html에서 Controller로 넘어올 때 파일을 담는 용도
+    private String originalFileName; // 원본 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름 (중복된 파일 이름 방지)
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
